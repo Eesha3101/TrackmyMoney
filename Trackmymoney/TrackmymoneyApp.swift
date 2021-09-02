b73e1d5ebddb45a69b492cd73015fbd3a26d4d0c
Eesha <eeshajpatel@gmail.com>
2021-09-02 12:31:19 -0400

    Initial Commit

---
+++ Trackmymoney/TrackmymoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackmymoneyApp.swift
+//  Trackmymoney
+//
+//  Created by Eesha Patel on 2021-09-02.
+//
+
+import SwiftUI
+
+@main
+struct TrackmymoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
